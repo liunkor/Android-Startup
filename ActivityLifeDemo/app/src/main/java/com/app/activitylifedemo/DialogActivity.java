2f0e3b0d6c1b647ca792007e1302dc82d25e0dff
liunkor <303507643@qq.com>
2015-06-13 20:45:53 +0800

    ActivityLife Demo

---
+++ ActivityLifeDemo/app/src/main/java/com/app/activitylifedemo/DialogActivity.java
@@ -0,0 +1,16 @@
+package com.app.activitylifedemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2015/6/5.
+ */
+public class DialogActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_layout);
+    }
+}
