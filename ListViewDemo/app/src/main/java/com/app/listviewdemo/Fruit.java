1b071dc07e3d3806e10b4cc2918e2aafb08a8f17
liunkor <303507643@qq.com>
2015-06-13 21:01:00 +0800

    ListView Demo

---
+++ ListViewDemo/app/src/main/java/com/app/listviewdemo/Fruit.java
@@ -0,0 +1,23 @@
+package com.app.listviewdemo;
+
+/**
+ * Created by Administrator on 2015/6/8.
+ */
+public class Fruit {
+    private String name;
+    private int imageid;
+
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageid = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageid() {
+        return imageid;
+    }
+
+}
